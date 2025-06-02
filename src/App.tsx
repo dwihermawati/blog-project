0ef8e4ce1b014cf468041e04d5e9426190ba91e3
dwihermawati <hermawatidwi1997@gmail.com>
2025-06-02 13:27:56 +0700

    feat(routing): Setup basic React Router DOM for Home, Login, and Register pages with initial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import './index.css';
+
 function App() {
-  return <div className='display-3xl-semibold'>test config tailwind</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
